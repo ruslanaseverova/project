88e807c40ba787e7712f44f580278c02dc732d33
ruslanaseverova <ruslanaseverova543@gmail.com>
2026-05-09 23:23:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,24 +22,26 @@ def send_welcome(message):
 def handle_document(message):
     doc = message.document
     # Получаем информацию о файле и скачиваем его
-    #file_info = bot.get_file(doc.file_id)
-    #file_bytes = bot.download_file(file_info.file_path)
-    #data = file_bytes.read()
-    #fname = doc.file_name.lower()
-    #try:
+    file_info = bot.get_file(doc.file_id)
+    file_bytes = bot.download_file(file_info.file_path)
+    data = file_bytes.read()
+    fname = doc.file_name.lower()
+    try:
         # В зависимости от расширения вызываем соответствующую функцию
-        #if fname.endswith(".pdf"):
-            #text = extract_text_from_pdf(data)
-        #elif fname.endswith(".docx"):
-            #text = extract_text_from_docx(data)
-        #elif fname.endswith(".txt"):
-            #text = data.decode(errors="ignore")
-        #else:
-            #message.reply("Формат не поддерживается. Поддерживаются: pdf, docx, txt.")
-            #return
-    #except Exception as e:
-        #message.reply(f"Ошибка при чтении файла: {e}")
-        #return
+        if fname.endswith(".pdf"):
+            text = extract_text_from_pdf(data)
+        elif fname.endswith(".docx"):
+            text = extract_text_from_docx(data)
+        elif fname.endswith(".txt"):
+            text = data.decode(errors="ignore")
+        else:
+            text ="Формат не поддерживается. Поддерживаются: pdf, docx, txt."
+            bot.reply_to(message, text)
+            return
+    except Exception as e:
+        text = f"Ошибка при чтении файла: {e}"
+        bot.reply_to(message, text)
+        return
     #res = score_resume(text)
     #resp = (
         #f"*Оценка*: {res['score']} / 100\n"
@@ -48,12 +50,8 @@ def handle_document(message):
         #f"*Email*: {', '.join(res['emails']) or 'не найден'}\n"
        # f"*Телефон*: {', '.join(res['phones']) or 'не найден'}\n"
         #f"*Навыки*: {', '.join(res['skills']) or 'не найдены'}\n"
-        #f"*Примечания*: {'; '.join(res['reasons'])}"
-    #)
-    #message.reply(resp)
-
-
-
+        #f"*Примечания*: {'; '.join(res['reasons'])}")
+    #bot.reply_to(message, resp)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
